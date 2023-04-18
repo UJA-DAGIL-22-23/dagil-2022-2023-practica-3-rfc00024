b73e8a1e769a8897f1d59575479e4f18108cbaf4
rfc00024 <rfc00024@red.ujaen.es>
2023-04-18 13:35:56 +0200

    añadido el expect de plantilla.imprimePersona  para la HU12 y HU13

--- front-end/static-files/js/ms-plantilla-spec.js
+++ front-end/static-files/js/ms-plantilla-spec.js
@@ -844,6 +844,44 @@ describe("personaComoFormulario2", function() {
 
 
 
+describe('Plantilla.imprimePersona', () => {
+  const persona = {
+    ref: { "@ref": { id: "ref persona 2" } },
+    data: { 
+      nombre: "Persona 1",
+      apellidos: "Apellidos",
+      participaciones_en_competiciones_oficiales: [2005,2006,2007,2009,2010,2012,2013,2014],
+      Participaciones_en_eventos_a_nivel_internacional: 3,
+      numero_de_trofeos_conseguidos: 2,
+    }
+  };
+
+  let actualizarSpy;
+  let almacenaDatosSpy;
+
+  beforeEach(() => {
+    actualizarSpy = spyOn(Frontend.Article, 'actualizar');
+    almacenaDatosSpy = spyOn(Plantilla, 'almacenaDatos');
+  });
+
+  it('debe llamar a la función Frontend.Article.actualizar', () => {
+    Plantilla.imprimePersona(persona);
+    expect(actualizarSpy).toHaveBeenCalled();
+  });
+
+  it('debe llamar a la función Plantilla.almacenaDatos', () => {
+    Plantilla.imprimePersona(persona);
+    expect(almacenaDatosSpy).toHaveBeenCalled();
+  });
+
+  it('debe llamar a la función Plantilla.personaComoFormulario', () => {
+    spyOn(Plantilla, 'personaComoFormulario');
+    Plantilla.imprimePersona(persona);
+    expect(Plantilla.personaComoFormulario).toHaveBeenCalledWith(persona);
+  });
+});
+
+
 
 
 
